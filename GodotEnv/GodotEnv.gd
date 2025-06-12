7ecaeb7729e2eba464c639dbdd08d786fa1b60b0
Ivan Preziosi <ivan.preziosi@gmail.com>
2025-06-12 14:39:20 +0200

    Added extensive comments

--- GodotEnv/GodotEnv.gd
+++ GodotEnv/GodotEnv.gd
@@ -1,19 +1,31 @@
 @tool
 extends EditorPlugin
 
-# Name of the Singleton in the project
+# --- Plugin Configuration Constants ---
+
+# Define the name under which the EnvManager will be registered as an autoload singleton.
+# This name is used to access the singleton globally (e.g., `DotEnv.get_var("MY_VAR")`).
 const SINGLETON_NAME = "DotEnv"
-# Path to main class script
+
+# Specify the relative path to the main `EnvManager` class script.
 const SINGLETON_PATH = "./EnvManager.gd"
 
 
+# --- Plugin Lifecycle Methods ---
+
+# Called when the plugin is enabled in the Godot editor.
 func _enable_plugin() -> void:
-	# Add autoload singleton
+	# Add the `EnvManager` script as an autoload singleton to the project.
 	add_autoload_singleton(SINGLETON_NAME, SINGLETON_PATH)
+
+	# Print a confirmation message to the Godot output console
 	print("Plugin goDotEnv activated: singleton '%s' added." % SINGLETON_NAME)
 
 
+# Called when the plugin is disabled or removed from the Godot editor.
 func _disable_plugin() -> void:
-	# Remove autoload singleton
+	# Remove the previously added autoload singleton.
 	remove_autoload_singleton(SINGLETON_NAME)
+
+	# Print a confirmation message to the Godot output console
 	print("Plugin goDotEnv deactivated: singleton '%s' removed." % SINGLETON_NAME)
